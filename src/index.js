fedfc8128fec081336d3f97c856d0eaf6e45e8f7
Igor Pavlichenko <igor.pavlichenko@comon.pt>
2017-04-27 10:52:45 +0100

    Make Board store state of each Square
    Use value property to reflect state when rendering square.
    Display value property inside Square's button.

--- src/index.js
+++ src/index.js
@@ -14,15 +14,22 @@ class Square extends React.Component {
 	render() {
 		return (
 			<button className="square" onClick={() => this.setState({value: 'X'}) }>
-				{this.state.value}
+				{this.props.value}
 			</button>
 		);
 	}
 }
 
 class Board extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			squares: Array(9).fill(null),
+		};
+	}
+
 	renderSquare(i) {
-		return <Square value={i} />;
+		return <Square value={this.state.squares[i]} />;
 	}
 	render() {
 		const status = 'Next player: X';
